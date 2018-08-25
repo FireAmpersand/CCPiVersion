b11dc4f2d8f688976b3455f9f3826e951d3e6724
Zachary <fireampersand@live.com>
2018-08-24 21:06:59 -0600

    Added a main file that just runs the terminal and the game

---
+++ textRunner.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cc_pi_gui_version;
+
+/**
+ *
+ * @author firea
+ */
+public class textRunner {
+    public static void main(String[] args){
+        Game theGame = new Game();
+        theGame.startNewGame();
+    }
+}
